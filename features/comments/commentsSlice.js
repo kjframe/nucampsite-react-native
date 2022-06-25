0dfcc7725e568d5ac5965eabc3744e4bbc7c333f
Kyle Frame <kjframe1223@gmail.com>
2022-06-25 12:02:05 -0500

    Created a Modal that contains a form to allow users to submit a rating and comment text about a campsite, along with an author name. Modal is able to be toggled open from a pencil icon. User able to update the comment state in the redux store

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -9,10 +9,28 @@ export const fetchComments = createAsyncThunk(
     }
 );
 
+export const postComment = createAsyncThunk (
+    'comments/postComment',
+    async (payload, {dispatch, getState }) => {
+        setTimeout(() => {
+            const { comments } = getState();
+            const date = new Date();
+            const id = comments.commentsArray.length; 
+            payload.date = date.toISOString();
+            payload.id = id;
+            dispatch(addComment(payload));
+        }, 2000);
+    }
+)
+
 const commentsSlice = createSlice({
     name: 'comments',
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
+    reducers: {
+        addComment: (state, action) => {
+            state.commentsArray.push(action.payload); 
+        }
+    },
     extraReducers: {
         [fetchComments.pending]: (state) => {
             state.isLoading = true;
@@ -28,5 +46,7 @@ const commentsSlice = createSlice({
         }
     }
 });
+
+export const { addComment } = commentsSlice.actions;
 
 export const commentsReducer = commentsSlice.reducer;
